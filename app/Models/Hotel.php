c2280f38ed8a3e29335d88f3aad32af394e8f34a
Jesuskor <missa-perez@hotmail.com>
2026-01-19 17:52:28 -0600

    feat(hotel): definir esquema de la tabla

--- app/Models/Hotel.php
+++ app/Models/Hotel.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Hotel extends Model
 {
-    //
+    protected $fillable = ['name', 'city', 'address', 'star_rating', 'price_per_night', 'image_url'];
 }
--- database/migrations/2026_01_19_234903_create_hotels_table.php
+++ database/migrations/2026_01_19_234903_create_hotels_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('hotels', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('city')->index();
+            $table->string('address');
+            $table->unsignedTinyInteger('star_rating')->default(0);
+            $table->decimal('price_per_night', 8, 2);
+            $table->string('image_url')->nullable();
             $table->timestamps();
         });
     }
