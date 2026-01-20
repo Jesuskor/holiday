ecbaa6c681144e770908e83a998fbb09e23791b9
Jesuskor <missa-perez@hotmail.com>
2026-01-19 22:06:39 -0600

    feat(reservations): devolver la información del hotel a reservar a la vista

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Hotel;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ReservationController extends Controller
 {
-    //
+    public function create(Hotel $hotel) {
+        return Inertia::render('reservations/Create', ['hotel' => $hotel]);
+    }
 }
