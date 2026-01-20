5249cc4737649273f81765607f584fc96bd022bc
Jesuskor <missa-perez@hotmail.com>
2026-01-19 22:03:11 -0600

    feat: implementar la nueva ruta de reservacion de hotel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HotelController;
+use App\Http\Controllers\ReservationController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
@@ -16,5 +17,6 @@ Route::get('dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('hotels', [HotelController::class, 'index'])->middleware(['auth', 'verified'])->name('hotels.index');
+Route::get('hotels/{hotel}/reserve', [ReservationController::class, 'create'])->middleware(['auth', 'verified'])->name('reservations.create');
 
 require __DIR__.'/settings.php';
