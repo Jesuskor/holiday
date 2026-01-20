e168039098129a1faf8b72a6810bbd65306fb5ce
Jesuskor <missa-perez@hotmail.com>
2026-01-20 16:34:11 -0600

    feat(reservations): crear form request para validar la creación de reservas

---
+++ app/Http/Requests/Reservation/StoreReservationRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Reservation;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreReservationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'hotel_id'      => 'required|exists:hotels,id',
+            'guest_name'    => 'required|string|max:255',
+            'guest_email'   => 'required|email',
+            'check_in_date' => 'required|date|after_or_equal:today',
+            'check_out_date'=> 'required|date|after:check_in_date',
+            'guest_count'   => 'required|integer|min:1',
+        ];
+    }
+}
